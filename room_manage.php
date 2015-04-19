bc467fa428269d4e09e3e4bfdc8c0cdf794cdd01
vorabhol <vorabhol.c@ku.th>
2015-04-19 15:02:53 +0700

    edit database struvture

--- room_manage.php
+++ room_manage.php
@@ -1,27 +1,29 @@
 <!DOCTYPE html>
  <table border=1>
+
      <tr>
      <th>RoomNumber</th>
      <th>status</th> 
-     <th>Deposit</th>
-     <th>Date_IN</th>
-     <th>Date_Out</th>
-     <th>User</th>
-     <th>Password</th>
+     <th>Deposit</th>   
+    
      <th>Type</th>
      <th>Price</th>
      </tr>
    <form method="POST" action"{$_SERVER['PHP_SELF']}">
     <tr>
     <td><input type="text" name="uroom_Number"></td>
-    <td><input type="text" name="ustatus"></td>
-    <td><input type="text" name="udeposit"></td> 
-   <td><input type="text" name="udatein"></td> 
-   <td><input type="text" name="udateout"></td> 
-  <td><input type="text" name="uuser"></td>
-  <td><input type="text" name="upassword"></td>
-  <td><input type="text" name="utype"></td>
-  <td><input type="text" name="uprice"></td>
+    
+    <td><input type="radio" name="ustatus" value="ready" checked>ready        
+         <input type="radio" name="ustatus" value="stay" >stay
+    </td>   
+
+    <td><input type="text" name="udeposit" value="0"></td>
+    <td><input type="radio" name="utype" value="daily" checked>daily
+        <input type="radio" name="utype" value="monthly" >monthly
+
+    </td>
+   <td><input type="text" name="uprice"></td>   
+  
 <td><input type="submit" name="send" value="add"></td>
 </form>
 <meta charset="utf-8"/>
@@ -38,11 +40,9 @@ function show_form(){
 function process_form(){
   $roomNumber=trim($_POST["uroom_Number"]);
   $status=trim($_POST["ustatus"]);
-  $deposit=trim($_POST["udeposit"]);
-  $datein=trim($_POST["udatein"]);
-  $dateout=trim($_POST["udateout"]);
-  $user=trim($_POST["uuser"]);
-  $password=trim($_POST["upassword"]);
+  $deposit=trim($_POST["udeposit"]); 
+  $user=$roomNumber;
+  $password=$roomNumber;
   $type=trim($_POST["utype"]);
   $price=trim($_POST["uprice"]);
   $roomNumber=addslashes($roomNumber);
@@ -61,12 +61,15 @@ function process_form(){
 
   mysql_select_db("domitaryproject");  
   
-  $sql= "INSERT INTO room VALUES ('{$roomNumber}','{$status}',{$deposit},'{$datein}','{$dateout}','{$user}','{$password}','{$type}',{$price});";
+  echo md5("2560");
+  $sql= "INSERT INTO room VALUES ({$roomNumber},'{$status}',{$deposit},'{$type}',{$price});";
+  $sql2="INSERT INTO  users VALUES ('{$user}',md5('{$password}'),0)";
   
   $result=mysql_query($sql,$con);
+  $result2=mysql_query($sql2,$con);
   echo "$result";
   echo "<h3>result </h3>\n";
-  if($result)
+  if($result&&$result2)
     echo "have been add".mysql_affected_rows($con);  
   else  
     echo" Error";
