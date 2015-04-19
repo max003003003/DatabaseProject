f14689dd17bf948a45bfb373f14b37c75875a336
vorabhol <vorabhol.c@ku.th>
2015-04-19 12:41:35 +0700

    add data to DB succes bug with single qoute

--- room_manage.php
+++ room_manage.php
@@ -1,32 +1,8 @@
 <!DOCTYPE html>
-<meta charset="utf-8"/>
-<body>
-<?php
-
-	$con=mysql_connect("localhost","root","root");
-    if(!$con )
-    {
-    	die("connot connect: ".mysql_error());
-    }
-
-    mysql_select_db("domitaryproject");
-   
-
-     if (isset($_POST['add'])){
-        $addQuery= "INSERT INTO  room (Room_Number,Status, Deposit ,Date_IN ,Date_Out ,User, Password ,Type ,Price)
-        VALUES ('$_POST[uroom_Number]','$_POST[ustatus]','$_POST[udeposit]','$_POST[udate_IN]','$_POST[udate_Out]','$_POST[uuser]','$_POST[upassword]','$_POST[utype]','$_POST[uprice]' )";
-       echo $addQuery;
-
-        mysql_query($addQuery,$con);
-     };
-    
-
-     $sql= "SELECT *FROM room ";
-     $mydata=mysql_query($sql,$con);
-     echo "<table border=1>
+ <table border=1>
      <tr>
      <th>RoomNumber</th>
-     <th>status</th>
+     <th>status</th> 
      <th>Deposit</th>
      <th>Date_IN</th>
      <th>Date_Out</th>
@@ -34,26 +10,73 @@
      <th>Password</th>
      <th>Type</th>
      <th>Price</th>
-     </tr>"; 
-  
-  echo "<form action=room_manage.php method=post>";
-  echo "<tr>";
-  echo "<td><input type=text name=uroom_Number></td>";
+     </tr>
+   <form method="POST" action"{$_SERVER['PHP_SELF']}">
+    <tr>
+    <td><input type="text" name="uroom_Number"></td>
+    <td><input type="text" name="ustatus"></td>
+    <td><input type="text" name="udeposit"></td> 
+   <td><input type="text" name="udatein"></td> 
+   <td><input type="text" name="udateout"></td> 
+  <td><input type="text" name="uuser"></td>
+  <td><input type="text" name="upassword"></td>
+  <td><input type="text" name="utype"></td>
+  <td><input type="text" name="uprice"></td>
+<td><input type="submit" name="send" value="add"></td>
+</form>
+<meta charset="utf-8"/>
+<?php
+  if(isset($_POST["send"]))
+       process_form(); 
+  else
+    show_form();
 
-  echo "<td><input type=float name=udeposit></td>"; 
-  echo "<td><input type=text name=uuser</td>";
-  echo "<td><input type=text name=upassword</td>";
-  echo "<td><input type=text name=utype></td>";
-  echo "<td><input type=text name=uprice</td>";
-  echo "<td>"."<input type=submit name=add value=add" . " </td>";
-  
+function show_form(){ 
+ 
+}
 
+function process_form(){
+  $roomNumber=trim($_POST["uroom_Number"]);
+  $status=trim($_POST["ustatus"]);
+  $deposit=trim($_POST["udeposit"]);
+  $datein=trim($_POST["udatein"]);
+  $dateout=trim($_POST["udateout"]);
+  $user=trim($_POST["uuser"]);
+  $password=trim($_POST["upassword"]);
+  $type=trim($_POST["utype"]);
+  $price=trim($_POST["uprice"]);
+  $roomNumber=addslashes($roomNumber);
+  $status=addslashes($status);
+  $deposit=doubleval($deposit);
+  $user=addslashes($user);
+  $password=addslashes($password);
+  $type=addslashes($type);
+  $price=doubleval($price);  
+   
+  $con=mysql_connect("localhost","root","root");
+    if(!$con )
+    {
+      die("connot connect: ".mysql_error());
+    }
+
+  mysql_select_db("domitaryproject");  
   
-  echo "</form>";  
-  echo "</table>";
+  $sql= "INSERT INTO room VALUES ('{$roomNumber}','{$status}',{$deposit},'{$datein}','{$dateout}','{$user}','{$password}','{$type}',{$price});";
+  
+  $result=mysql_query($sql,$con);
+  echo "$result";
+  echo "<h3>result </h3>\n";
+  if($result)
+    echo "have been add".mysql_affected_rows($con);  
+  else  
+    echo" Error";
+    
   mysql_close($con);
-  
+}
+
 
 ?>
+<body>
+
 </body>
 </html>
